7570fb5c7fce65fc270fddb6e2fd0d0940085ec9
HashCode01 <ssrohan99@gmail.com>
2021-03-10 11:53:13 +0530

    Added Code Base

---
+++ dashboard/src/components/GlobalHeader/AvatarDropdown.jsx
@@ -0,0 +1,62 @@
+import { Avatar, Icon, Menu, Spin } from 'antd';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+import React from 'react';
+import { connect } from 'dva';
+import { router } from 'umi';
+import HeaderDropdown from '../HeaderDropdown';
+import styles from './index.less';
+
+class AvatarDropdown extends React.Component {
+  onMenuClick = event => {
+    const { key } = event;
+
+    if (key === 'logout') {
+      const { dispatch } = this.props;
+
+      if (dispatch) {
+        dispatch({
+          type: 'login/logout',
+        });
+      }
+
+      return;
+    }
+
+    router.push(`/account/${key}`);
+  };
+
+  render() {
+    const {
+      currentUser = {
+        avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+        name: 'SkyTeam',
+      }
+    } = {};
+    const menuHeaderDropdown = (
+      <Menu className={styles.menu} selectedKeys={[]} >
+        <Menu.Item key="logout">
+          <Icon type="logout" />
+          <FormattedMessage id="menu.account.logout" defaultMessage="logout" />
+        </Menu.Item>
+      </Menu>
+    );
+    return currentUser && currentUser.name ? (
+      <HeaderDropdown overlay={menuHeaderDropdown}>
+        <span className={`${styles.action} ${styles.account}`}>
+          <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
+          <span className={styles.name}>{currentUser.name}</span>
+        </span>
+      </HeaderDropdown>
+    ) : (
+      <Spin
+        size="small"
+        style={{
+          marginLeft: 8,
+          marginRight: 8,
+        }}
+      />
+    );
+  }
+}
+
+export default AvatarDropdown;
